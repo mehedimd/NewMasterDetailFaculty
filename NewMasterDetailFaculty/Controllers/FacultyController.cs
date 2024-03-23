4f5e8fec706654817d5261e784f16ac88b314b6b
mehedimd <mehedi105662@gmail.com>
2024-03-23 10:48:11 +0600

    Add project files.

---
+++ NewMasterDetailFaculty/Controllers/FacultyController.cs
@@ -0,0 +1,114 @@
+﻿using NewMasterDetailFaculty.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewMasterDetailFaculty.Controllers
+{
+    public class FacultyController : Controller
+    {
+        NewFacultyStudentDBEntities db = new NewFacultyStudentDBEntities();
+        // GET: Faculty
+        public ActionResult Index()
+        {            
+            return View(db.Faculties.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Faculty faculty)
+        {
+            if (ModelState.IsValid && faculty.Students != null && faculty.Students.Count > 0)
+            {
+                if (faculty.Picture != null)
+                {
+                    var ext = Path.GetExtension(faculty.Picture.FileName);
+                    if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".svg")
+                    {
+                        var rootPath = Server.MapPath("~/");
+                        var fileToSave = Path.Combine(rootPath, "Pictures", faculty.Picture.FileName);
+                        faculty.Picture.SaveAs(fileToSave);
+                        faculty.PicPath = "~/Pictures/" + faculty.Picture.FileName;
+                        db.Faculties.Add(faculty);
+                        if (db.SaveChanges() > 0)
+                        {
+                            return RedirectToAction("Index");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Please Provide a Valid Error");
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("","Please Add atleast 1 Student");
+                return View(faculty);
+            }
+            return View(faculty);
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(db.Faculties.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Faculty faculty)
+        {
+            var oldFaculty = db.Faculties.Find(faculty.ID);
+            if (ModelState.IsValid && faculty.Students != null && faculty.Students.Count > 0)
+            {
+                if (faculty.Picture != null)
+                {
+                    var ext = Path.GetExtension(faculty.Picture.FileName);
+                    if (ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".svg")
+                    {
+                        var rootPath = Server.MapPath("~/");
+                        var fileToSave = Path.Combine(rootPath, "Pictures", faculty.Picture.FileName);
+                        faculty.Picture.SaveAs(fileToSave);
+                        faculty.PicPath = "~/Pictures/" + faculty.Picture.FileName;
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Please Provide a Valid Error");
+                    }
+                }
+                else
+                {
+                    oldFaculty.PicPath = oldFaculty.PicPath;
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Please Add atleast 1 Student");
+                return View(faculty);
+            }
+            oldFaculty.FacultyName = faculty.FacultyName;
+            oldFaculty.CourseName = faculty.CourseName;
+            oldFaculty.PhoneNumber = faculty.PhoneNumber;
+            oldFaculty.CourseStartDate = faculty.CourseStartDate;
+            oldFaculty.IsRunning = faculty.IsRunning;
+            db.Students.RemoveRange(db.Students.Where(s => s.FacultyID.Equals(oldFaculty.ID)));
+            db.SaveChanges();
+            oldFaculty.Students = faculty.Students;
+            db.Entry(oldFaculty).State = System.Data.Entity.EntityState.Modified;
+            if (db.SaveChanges() > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(faculty);
+        }
+        // Delete Action
+        public ActionResult Delete(int id)
+        {
+            db.Faculties.Remove(db.Faculties.Find(id));
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NewMasterDetailFaculty/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NewMasterDetailFaculty")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NewMasterDetailFaculty")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("824bd60c-e873-4fb8-a008-264c82f78890")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
